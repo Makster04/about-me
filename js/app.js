b7e7c739fce23a78a6f2700a4b4301e5bd65d631
Makster04 <86382359+Makster04@users.noreply.github.com>
2024-01-23 23:18:31 -0800

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,57 @@
+// Question 1
+let question1 = prompt('Am I tall? (y/n)');
+let answer1 = 'y';
+
+if (question1 === 'y')  {
+    console.log('y');
+} else {
+    alert('You are wrong!');
+    console.log('Wrong')
+    
+}
+
+// Question 2
+let question2 = prompt('Am I new to coding? (y/n)');
+let answer2 = 'y';
+
+if (question2 === 'y')  {
+    console.log('y');
+} else {
+    alert('You are wrong!');
+    console.log('Wrong')
+}
+
+// Question 3
+let question3 = prompt('Do I take the bus to my coding bootcamp? (y/n)');
+let answer3 = 'y';
+
+if (question3 === 'y')  {
+    console.log('y');
+} else {
+    alert('You are wrong!');
+    console.log('Wrong')
+}
+
+// Question 4
+let question4 = prompt('Do I live in Bellevue? (y/n)');
+let answer4 = 'y';
+
+if (question4 === 'y')  {
+    console.log('y');
+} else {
+    alert('You are wrong!');
+    console.log('Wrong!')
+}
+
+// Question 5
+let question5 = prompt('Do I go to the gym? (y/n)')
+let answer5 = 'y'
+
+if (question5 === 'y')  {
+    console.log('y');
+} else {
+    alert('You are wrong!');
+    console.log('Wrong!')
+}
+let question6 = prompt('What is your name?');
+document.write('<p> Hello!' + question6 + '</p>');
