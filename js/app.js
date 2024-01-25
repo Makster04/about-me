a5ab7190f4ac8c4ac9389bbd43afb2a372bdd79b
Makster04 <86382359+Makster04@users.noreply.github.com>
2024-01-24 21:19:33 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,57 +1,102 @@
-// Question 1
-let question1 = prompt('Do I have a Balkan Heritage? (y/n)');
-let answer1 = 'y';
-
-if (question1 === 'y')  {
-    console.log('y');
-} else {
-    alert('You are wrong!');
-    console.log('Wrong')
-    
+function buttonGame() {
+    let userName = "Welcome welcome! What do you call yourself?!";
+    let user = prompt(userName);
+    let answerString1 = "Whatsupp " + user + "! Do you know anything about me?!";
+    alert(answerString1);
+    let userConfirmed;
+    do {
+        userConfirmed = prompt("Are you readdyyyyyyyyy? (y/n)");
+        userConfirmed = userConfirmed ? userConfirmed.toLowerCase() : "";
+    } while (userConfirmed !== "y" && userConfirmed !== "n");
+    if (userConfirmed !== "y") {
+        alert("Aight, see you later?");
+        return;
+    }
+    let questions = [
+        "What month was I born?",
+        "What college did I go to (2 letter abbreviation)?",
+        "How old was I in 2009?",
+        "Which city do I live in?",
+        "Which map region is my family from?",
+        "Who is my favorite Basketball Player?"
+    ];
+    let correctAnswers = [
+        "april",
+        "UW",
+        "11",
+        "bellevue",
+        "balkans",
+        "Nikola Jokic"
+    ];
+    let answerChoices = [
+        ["april"],
+        ["UW"],
+        ["11"],
+        ["bellevue"],
+        Array.from({ length: 11 }, (_, i) => (i + 15).toString()),
+        ["Nikola Jokic", "Shai Gilgeous-Alexander", "Tyrese Haliburton", "LeBron James", "Luka Doncic", "DeArron Fox", "Alperen Sengun", "Victor Wembanyama", "Kawhi Leonard", "Anthony Edwards"]
+    ];
+    let correctCount = 0;
+    let wrongCount = 0;
+    for (let i = 0; i < questions.length; i++) {
+        let userAnswer = prompt(questions[i]);
+        let lowercasedAnswer = userAnswer.toLowerCase();
+        let correctAnswer = correctAnswers[i].toLowerCase();
+        if (i === 4) {
+            // Numeric Validation
+            let userGuessNumber = parseInt(userAnswer);
+            if (isNaN(userGuessNumber)) {
+                alert("Please enter a valid number.");
+                break;
+            }
+            let favoriteNumber = 7;
+            let remainingTriesNumber = 4;
+            while (remainingTriesNumber > 0) {
+                if (userGuessNumber === favoriteNumber) {
+                    alert("CORRECT!");
+                    correctCount++;
+                    break;
+                } else {
+                    remainingTriesNumber--;
+                    alert("WRONG! " + remainingTriesNumber + " tries left.");
+                    if (remainingTriesNumber > 0) {
+                        userAnswer = prompt(questions[i]);
+                        userGuessNumber = parseInt(userAnswer);
+                    }
+                }
+            }
+        } else if (i === 5) {
+            // Nickname Comparison in Question 5
+            let remainingTriesNickname = 6;
+            while (remainingTriesNickname > 0) {
+                let userGuessNickname = prompt(
+                    questions[i] + "\nAvailable choices: " + answerChoices[i].join(", ")
+                );
+                if (!answerChoices[i].includes(userGuessNickname)) {
+                    alert("Invalid choice. Please choose from the available options.");
+                    continue;
+                }
+                if (userGuessNickname.toLowerCase() === correctAnswer) {
+                    alert("Yay, you guessed correctly!");
+                    correctCount++;
+                    break;
+                } else {
+                    remainingTriesNickname--;
+                    alert("WRONGGGG! You have " + remainingTriesNickname + " tries left.");
+                    answerChoices[i] = answerChoices[i].filter(choice => choice !== userGuessNickname);
+                }
+            }
+        } else {
+            if (lowercasedAnswer === correctAnswer) {
+                let confirmation = "Give me a high five " + user + ", You nailed it!!";
+                alert(confirmation);
+                correctCount++;
+            } else {
+                alert("Better luck on the next one :(" + user + "");
+                wrongCount++;
+            }
+        }
+    }
+    let finalMessage = "Thanks for giving it a go, " + user + "!\nYou got " + correctCount + " right and " + wrongCount + " wrong!!!";
+    alert(finalMessage);
 }
-
-// Question 2
-let question2 = prompt('Am I new to coding? (y/n)');
-let answer2 = 'y';
-
-if (question2 === 'y')  {
-    console.log('y');
-} else {
-    alert('You are wrong!');
-    console.log('Wrong')
-}
-
-// Question 3
-let question3 = prompt('Am I unemployed at the moment? (y/n)');
-let answer3 = 'y';
-
-if (question3 === 'y')  {
-    console.log('y');
-} else {
-    alert('You are wrong!');
-    console.log('Wrong')
-}
-
-// Question 4
-let question4 = prompt('Do I live in Bellevue? (y/n)');
-let answer4 = 'y';
-
-if (question4 === 'y')  {
-    console.log('y');
-} else {
-    alert('You are wrong!');
-    console.log('Wrong!')
-}
-
-// Question 5
-let question5 = prompt('Do I go to the gym? (y/n)')
-let answer5 = 'y'
-
-if (question5 === 'y')  {
-    console.log('y');
-} else {
-    alert('You are wrong!');
-    console.log('Wrong!')
-}
-let question6 = prompt('What is your name?');
-document.write('<p> Hello!' + question6 + '</p>');
